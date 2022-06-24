86d0afac515eb5530589251798defc750ea223b9
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-06-24 09:35:23 +0300

    Add files via upload

---
+++ python_core_2/task05.py
@@ -0,0 +1,8 @@
+def alphabet_soup(word):
+    return "".join(sorted(word))
+
+
+print(alphabet_soup("hello"))
+print(alphabet_soup("hacker"))
+print(alphabet_soup("geek"))
+print(alphabet_soup("javascript"))
